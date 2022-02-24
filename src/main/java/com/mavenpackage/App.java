e4e6697ea7d063d7f1b0b44d27be33f628fe4a5c
UmmayAbiha <83369767+UmmayAbiha@users.noreply.github.com>
2022-02-24 11:57:50 +0530

    Add files via upload

---
+++ src/main/java/com/mavenpackage/App.java
@@ -0,0 +1,17 @@
+package com.mavenpackage;
+
+import java.util.Scanner;
+
+public class App {
+    public static void printHello()
+    {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        printHello();
+    }
+}
+
+
+
